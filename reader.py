5badb20c30ff6eb0514781ce37912016a49825c5
ChristianRommel <Christian.Rommel.90@gmail.com>
2016-12-12 18:23:42 +0100

    changed sentiment reader

--- reader.py
+++ reader.py
@@ -2,28 +2,35 @@ import csv
 import nltk
 import re
 from array import *
+punct = []
 stops = []
-obj1 = open('stopwords.txt', 'r')
+ntitle = []
+obj1 = open('punctuations.txt', 'r')
+for s in obj1:
+	#Lowercase words and strip newlines ans whitespaces
+	punctuations = s.rstrip()
+	punctuations = re.escape(punctuations)
+	punct.append(punctuations)
+obj1.close
+obj2 = open('stopwords.txt', 'r')
 for s in obj1:
 	#Lowercase words and strip newlines ans whitespaces
 	stopwords = s.lower().rstrip()
 	stopwords = re.escape(stopwords)
 	stops.append(stopwords)
-obj1.close
+obj2.close
 
 
 
 #Expressions
-rgx_list = ['\.', ',', ';', '\(', '\)', ':', '\.\.\.', '!']
-#New empty array
-ntitle = []
+#rgx_list = ['\.', ',', ';', '\(', '\)', ':', '\.\.\.', '!']
 #Open a csv
 with open('tripadvisor_dieburg.csv') as file: 	
 	reader = csv.DictReader(file)
 	#Get the title and replace the expressions	
 	for row in reader:
 		new_title = row['title'].lower()
-		for r in rgx_list:
+		for r in punct:
 			rgx = re.compile(r)
 			new_title = re.sub(rgx, '', new_title)
 		for r in stops:
