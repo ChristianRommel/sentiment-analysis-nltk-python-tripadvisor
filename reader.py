93a88ed5f500d786f01f0ef7d3771f9578444773
ChristianRommel <Christian.Rommel.90@gmail.com>
2016-12-19 20:36:48 +0100

    Simple Search is implemented

--- reader.py
+++ reader.py
@@ -7,15 +7,16 @@ stops = []
 ntitle = []
 with open('wordlist/punctuation.txt', 'r') as obj1:
 	for s in obj1:
-		#Lowercase words and strip newlines ans whitespaces
 		punctuations = s.rstrip()
 		punctuations = re.escape(punctuations)
+		punctuations = re.compile(punctuations)
 		punct.append(punctuations)
 with open('wordlist/stopwords.txt', 'r') as obj2:
 	for s in obj2:
 		#Lowercase words and strip newlines ans whitespaces
 		stopwords = s.lower().rstrip()
 		stopwords = re.escape(stopwords)
+		stopwords = re.compile("{}{}{}".format("\\b", stopwords, "\\b"))
 		stops.append(stopwords)
 #Open a csv to retrieve the reviews
 with open('tripadvisor_dieburg.csv') as file:
@@ -24,15 +25,16 @@ with open('tripadvisor_dieburg.csv') as file:
 	for row in reader:
 		new_title = row['title'].lower()
 		for r in punct:
-			rgx = re.compile(r)
-			new_title = re.sub(rgx, '', new_title)
+			# r = re.compile(r)
+			new_title = re.sub(r, '', new_title)
 		for r in stops:
-			patter = "{}{}{}".format("\\b", r, "\\b")
-			rgx = re.compile(patter)
-			new_title = re.sub(rgx, '', new_title)
+			#patter = "{}{}{}".format("\\b", r, "\\b")
+			#r = re.compile(patter)
+			new_title = re.sub(r, '', new_title)
 		#Tokenizer
 		new_title = nltk.word_tokenize(new_title)
 		#Append to the array
 		ntitle.append(new_title)
 #Print the new title
 #print ntitle[0][1]
+#re.purge()
