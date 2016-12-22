b1578f8d1901d84e7b57ca079b0d193f43706cb2
ChristianRommel <Christian.Rommel.90@gmail.com>
2016-12-22 20:36:32 +0100

    add content

--- reader.py
+++ reader.py
@@ -5,6 +5,7 @@ from array import *
 punct = []
 stops = []
 ntitle = []
+ncontent= []
 with open('wordlist/punctuation.txt', 'r') as obj1:
 	for s in obj1:
 		punctuations = s.rstrip()
@@ -24,19 +25,25 @@ with open('tripadvisor_dieburg.csv') as file:
 	#Get the title and replace the expressions
 	for row in reader:
 		new_title = row['title'].lower()
+		new_content = row['content'].lower()
 		#print new_title
 		for r in punct:
 			# r = re.compile(r)
 			new_title = re.sub(r, '', new_title)
+			new_content = re.sub(r, '', new_content)
 		for r in stops:
 			#patter = "{}{}{}".format("\\b", r, "\\b")
 			#r = re.compile(patter)
 			new_title = re.sub(r, '', new_title)
+			new_content = re.sub(r, '', new_content)
 		#Tokenizer
 		new_title = nltk.word_tokenize(new_title)
+		new_content = nltk.word_tokenize(new_content)
 		#Append to the array
 		ntitle.append(new_title)
+		ncontent.append(new_content)
 #Print the new title
 # print ntitle
 # print ntitle[0][1]
+#print ncontent
 #re.purge()
