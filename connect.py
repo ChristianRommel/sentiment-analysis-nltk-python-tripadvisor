7ed2ed85d0ac30f2984740a7210b1d329c772cca
ChristianRommel <Christian.Rommel.90@gmail.com>
2017-01-10 19:12:19 +0100

    add mongodb connector

---
+++ connect.py
@@ -0,0 +1,33 @@
+import pymongo
+from pymongo import MongoClient
+client = MongoClient('mongodb://localhost:27017/')
+db = client.sentimentdb
+
+from datetime import datetime
+result = db.restaurants.insert_one(
+    {
+        "address": {
+            "street": "2 Avenue",
+            "zipcode": "10075",
+            "building": "1480",
+            "coord": [-73.9557413, 40.7720266]
+        },
+        "borough": "Manhattan",
+        "cuisine": "Italian",
+        "grades": [
+            {
+                "date": datetime.strptime("2014-10-01", "%Y-%m-%d"),
+                "grade": "A",
+                "score": 11
+            },
+            {
+                "date": datetime.strptime("2014-01-16", "%Y-%m-%d"),
+                "grade": "B",
+                "score": 17
+            }
+        ],
+        "name": "Vella",
+        "restaurant_id": "41704620"
+    }
+)
+result.inserted_id
